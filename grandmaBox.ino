7c7f28b9a16a1dd8e6875c906f11a06b4944031b
Daniel Geraldino <danielgguerra2@gmail.com>
2019-04-17 10:15:40 -0300

    Alguns ajuste e adição da função que pegar o horario do remedioem string e converte para inteiro

--- grandmaBox.ino
+++ grandmaBox.ino
@@ -1,5 +1,4 @@
 #include <TimeAlarms.h>
-#include <Time.h>
 #include <SoftwareSerial.h>
 
 const int pinoRX = 3;
@@ -12,14 +11,18 @@ const int ledBox5 = 9;
 const int ledBox6 = 8;
 const int buzzer = 7;
 const int btnSentinel = 6;
-
-time_t horaBox1;
+int horaBox1[3],
+    horaBox2[3],
+    horaBox3[3],
+    horaBox4[3],
+    horaBox5[3],
+    horaBox6[3];
 
 SoftwareSerial bluetooth(pinoRX, pinoTX);
 
 void setup() {
-  // put your setup code here, to run once:
   bluetooth.begin(9600);
+  Serial.begin(9600);
   pinMode(ledBox1, OUTPUT);
   pinMode(ledBox2, OUTPUT);
   pinMode(ledBox3, OUTPUT);
@@ -28,33 +31,20 @@ void setup() {
   pinMode(ledBox6, OUTPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(btnSentinel, INPUT_PULLUP);
+
+  while(not(bluetooth.available())){
+    bluetooth.println("Favor envia horarios");
+    delay(500);
+  }
+  ConfBox();
+  HorarioDia();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (bluetooth.available())
-  {
-    String mensagem;
-    mensagem = MensagemSerial();
-    if(mensagem.length() != 0)
-    {
-      //se a mensagem começa com '-' é pq o App que cronizar o horario.
-      if (mensagem[0] == '-')
-      {
-        SicronizaHorario(mensagem);
-      }
-      //configurando o horario da box1
-      /*if (mensagem[0] == '!')
-      {
-         
-      }*/
-    }
-  }
-  HorarioDia();  
+ ConfBox();  
 }
 
-//Funçoes
-
+//Junta os caracteres em uma string e o "*" indentifica o final da frase
 String MensagemSerial()
 {
   char p;
@@ -75,8 +65,8 @@ String MensagemSerial()
   }
   return palavra;
 }
-
-void AletaLed(int led)
+//Pisca o led de uma determido led e o buzz
+int AletaLedBuzz(int led)
 {//for é apanas para teste.
   for (int i; i < 20;i++)
   {
@@ -86,7 +76,7 @@ void AletaLed(int led)
     delay(300);
   }
 }
-
+//Imprime hora e dia pela porta bluetooth
 void HorarioDia()
 {
   bluetooth.print(hour());
@@ -102,12 +92,57 @@ void HorarioDia()
   bluetooth.println(year());
   delay(1000);
 }
-
+//Seta o horario do arduino
 void SicronizaHorario(String mensagem)
 {
   mensagem.remove(0,1);
   setTime(mensagem.toInt() - 10800);
   bluetooth.println(mensagem);
 }
-
+//Função que recebe uma string de hora(HH:MM) e retorna HH
+int HoraRemedio(String mensagem)
+{
+  mensagem.remove(0,1);
+  String hora;
+  for (int i = 0; i < 2;i++)
+  {
+    hora.concat(mensagem[i]);
+  }
+  return hora.toInt();
+}
+//Função que recebe uma string de hora(HH:MM) e retorna MM
+int MinutoRemedio(String mensagem)
+{
+  mensagem.remove(0,4);
+  String minuto;
+  for (int i = 0;i < 2;i++)
+  {
+    minuto.concat(mensagem[i]);
+  }
+  return minuto.toInt();
+}
+//Configuração da Box
+void ConfBox()
+{
+  if (bluetooth.available())
+  {
+    String mensagem;
+    mensagem = MensagemSerial();
+    if(mensagem.length() != 0)
+    {
+      //se a mensagem começa com '-' é pq o App que sicronizar o horario.
+      if (mensagem[0] == '-')
+      {
+        SicronizaHorario(mensagem);
+      }
+      //Para configura a box1 a mensagem começa com '!'
+      if (mensagem[0] == '!')
+      {
+        horaBox1[0] = HoraRemedio(mensagem);
+        horaBox1[1] = MinutoRemedio(mensagem);
+        Alarm.alarmRepeat(horaBox1[0], horaBox1[1], 0, AletaLedBuzz(ledBox1)); 
+      }
+    }    
+  }
+}
 
